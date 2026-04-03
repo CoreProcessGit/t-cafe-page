aaf5b07669c68a56c7e18fcf4dca420816e6e1e7
CoreprocessGit <cp.content100@gmail.com>
2026-04-03 17:25:16 +0900

    Switch to BrowserRouter, remove hash from URLs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,7 +9,7 @@ import TermsOfService from './pages/TermsOfService';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/t-cafe-page">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,7 +18,7 @@ function App() {
         <Route path="/terms-of-service" element={<TermsOfService />} />
       </Routes>
       <Footer />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
